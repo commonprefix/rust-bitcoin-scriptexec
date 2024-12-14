34b076fbe65a8c55ae025a11496756e2e5b765fc
Orestis Alpos <oralpos@gmail.com>
2024-12-14 11:56:23 +0200

    Revert "implement parse_asm locally because it was removed from storopoli" because parse_asm is now added back.
    This reverts commit 275780744215b39d081eb737ee707f753b06dde4.

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,12 @@ extern crate alloc;
 extern crate core;
 
 use alloc::borrow::Cow;
-use bitcoin::{hex, opcodes};
 use core::cmp;
-use std::str::FromStr;
 
 use bitcoin::consensus::Encodable;
 use bitcoin::hashes::{hash160, ripemd160, sha1, sha256, sha256d, Hash};
 use bitcoin::opcodes::{all::*, Opcode};
-use bitcoin::script::{
-    self, Builder, Instruction, Instructions, PushBytes, Script, ScriptBuf, ScriptExt,
-};
+use bitcoin::script::{self, Instruction, Instructions, Script, ScriptBuf, ScriptExt};
 use bitcoin::sighash::SighashCache;
 use bitcoin::taproot::{self, TapLeafHash};
 use bitcoin::transaction::{self, Transaction, TxOut};
@@ -1042,148 +1038,4 @@ fn read_scriptint(item: &[u8], size: usize, minimal: bool) -> Result<i64, ExecEr
         script::Error::Serialization => todo!(),
         _ => unimplemented!(),
     })
-}
-
-pub fn parse_asm(asm: &str) -> Result<ScriptBuf, ParseAsmError> {
-    fn err(position: (usize, usize), kind: AsmParseErrorKind) -> ParseAsmError {
-        ParseAsmError { position, kind }
-    }
-
-    let mut buf = Vec::with_capacity(65);
-    let mut builder = Builder::new();
-    let mut words = iter_words(asm);
-    while let Some((pos, mut word)) = words.next() {
-        // We have this special case in our formatter.
-        if word == "OP_0" {
-            builder = builder.push_opcode(OP_PUSHBYTES_0);
-            continue;
-        }
-
-        if let Ok(op) = Opcode::from_str(word) {
-            // check for push opcodes
-            if op.code <= OP_PUSHDATA4.code {
-                let (next, push) = words
-                    .next()
-                    .ok_or(err(pos, AsmParseErrorKind::UnexpectedEOF))?;
-                if !try_parse_raw_hex(push, &mut buf) {
-                    return Err(err(next, AsmParseErrorKind::InvalidHex));
-                }
-
-                // NB our API doesn't actually allow us to make byte pushes with
-                // non-minimal length prefix, so we can only check and error if
-                // the user wants a non-minimal push
-                let expected_push_op = match buf.len() {
-                    n if n < opcodes::all::OP_PUSHDATA1.code as usize => Opcode::from(n as u8),
-                    n if n < 0x100 => opcodes::all::OP_PUSHDATA1,
-                    n if n < 0x10000 => opcodes::all::OP_PUSHDATA2,
-                    n if n < 0x100000000 => opcodes::all::OP_PUSHDATA4,
-                    _ => return Err(err(next, AsmParseErrorKind::PushExceedsMaxSize)),
-                };
-                if op != expected_push_op {
-                    return Err(err(pos, AsmParseErrorKind::NonMinimalBytePush));
-                }
-
-                let push = <&PushBytes>::try_from(&buf[..])
-                    .map_err(|_| err(next, AsmParseErrorKind::PushExceedsMaxSize))?;
-                builder = builder.push_slice(push);
-            } else {
-                builder = builder.push_opcode(op);
-            }
-            continue;
-        }
-
-        // Not an opcode, try to interpret as number or push.
-
-        if word.starts_with('<') && word.ends_with('>') {
-            word = &word[1..word.len() - 1];
-        }
-
-        // Try a number.
-        if let Ok(i) = i32::from_str(&word) {
-            builder = builder
-                .push_int(i)
-                .map_err(|_| err(pos, AsmParseErrorKind::PushExceedsMaxSize))?;
-            continue;
-        }
-
-        // Finally, try hex in various forms.
-        if word.starts_with("0x") {
-            word = &word[2..];
-        }
-
-        if try_parse_raw_hex(word, &mut buf) {
-            let push = <&PushBytes>::try_from(&buf[..])
-                .map_err(|_| err(pos, AsmParseErrorKind::PushExceedsMaxSize))?;
-            builder = builder.push_slice(push);
-        } else {
-            return Err(err(pos, AsmParseErrorKind::UnknownInstruction));
-        }
-    }
-
-    Ok(builder.into_script())
-}
-
-/// Try to parse raw hex bytes and push them into the buffer.
-fn try_parse_raw_hex(hex: &str, buf: &mut Vec<u8>) -> bool {
-    buf.clear();
-    let iter = match hex::HexToBytesIter::new(hex) {
-        Ok(i) => i,
-        Err(_) => return false,
-    };
-    for item in iter {
-        let item = match item {
-            Ok(i) => i,
-            Err(_) => return false,
-        };
-        buf.push(item);
-    }
-    true
-}
-
-/// Create an iterator over instruction words and their position in the file.
-fn iter_words(asm: &str) -> impl Iterator<Item = ((usize, usize), &str)> {
-    asm.lines().enumerate().flat_map(|(line_idx, line)| {
-        let content = line
-            .splitn(2, "#")
-            .next()
-            .unwrap()
-            .splitn(2, "//")
-            .next()
-            .unwrap();
-        content
-            .split_whitespace()
-            .enumerate()
-            .map(move |(word_idx, word)| ((line_idx, word_idx), word))
-    })
-}
-
-/// The different kinds of [AsmParseError] that can occur.
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[non_exhaustive]
-pub enum AsmParseErrorKind {
-    /// ASM ended unexpectedly.
-    UnexpectedEOF,
-    /// We were not able to interpret the instruction.
-    UnknownInstruction,
-    /// Invalid hexadecimal bytes.
-    InvalidHex,
-    /// Byte push exceeding the maximum size.
-    PushExceedsMaxSize,
-    /// ASM contains a byte push with non-minimal size prefix.
-    ///
-    /// This is not necessarily invalid, but we can't construct such pushes.
-    NonMinimalBytePush,
-}
-
-/// Error from parsing Script ASM.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ParseAsmError {
-    /// The position of the instruction that caused the error.
-    ///
-    /// The value is (line, word) with word incremented after
-    /// every chunk of whitespace.
-    pub position: (usize, usize),
-
-    /// The kind of error that occurred.
-    pub kind: AsmParseErrorKind,
 }
